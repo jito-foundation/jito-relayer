32baaad591a268938583db2847562f3155a729ee
Lucas B <lucas@jito.wtf>
2024-01-05 18:16:29 -0600

    pull in protobuf-src for building and new jito-protos

--- jito-protos/build.rs
+++ jito-protos/build.rs
@@ -1,6 +1,12 @@
 use tonic_build::configure;
 
 fn main() {
+    const PROTOC_ENVAR: &str = "PROTOC";
+    if std::env::var(PROTOC_ENVAR).is_err() {
+        #[cfg(not(windows))]
+        std::env::set_var(PROTOC_ENVAR, protobuf_src::protoc());
+    }
+
     configure()
         .compile(
             &[
