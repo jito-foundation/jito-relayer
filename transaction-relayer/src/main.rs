89095461ba786e814d7030affebfae7c396edc43
Jed <4679729+jedleggett@users.noreply.github.com>
2024-04-29 21:12:34 -0500

    wip

--- transaction-relayer/src/main.rs
+++ transaction-relayer/src/main.rs
@@ -81,7 +81,7 @@ struct Args {
     num_tpu_quic_servers: u16,
 
     /// Port to bind to for tpu quic fwd packets.
-    /// Make sure to set this to at least (num_tpu_fwd_quic_servers + 6) higher than tpu_quic_port,
+    /// Make sure to set this to at least (num_tpu_fwd_quic_servers + 6) higher than tpu_forward_quic_port,
     /// to avoid overlap any tpu forward ports with the normal tpu ports.
     /// TPU_FWD will bind to all ports in the range of (tpu_fwd_quic_port, tpu_fwd_quic_port + num_tpu_fwd_quic_servers).
     /// Open firewall ports for this entire range
@@ -555,16 +555,8 @@ fn main() {
         delay_packet_receiver,
         leader_cache.handle(),
         public_ip,
-        (0..args.num_tpu_quic_servers)
-            .map(|i| (args.tpu_quic_port + i..args.tpu_quic_port + i + 1).collect())
-            .collect(),
-        (0..args.num_tpu_quic_servers)
-            .map(|i| {
-                (args.tpu_quic_fwd_port + i * args.num_quic_endpoints
-                    ..args.tpu_quic_fwd_port + (i + 1) * args.num_quic_endpoints)
-                    .collect()
-            })
-            .collect(),
+        (args.tpu_quic_port..args.tpu_quic_port + args.num_tpu_quic_servers).collect(),
+        (args.tpu_quic_fwd_port..args.tpu_quic_fwd_port + args.num_tpu_fwd_quic_servers).collect(),
         health_manager.handle(),
         exit.clone(),
         ofac_addresses,
